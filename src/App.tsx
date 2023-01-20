761b3a28f38f5a8d8340b7933647a5ec89e9b566
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-01-21 01:05:56 +0530

    basic configs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
   return (
-    <div className="app">
-      app
+    <div className="app bg-gray-20">
+      <Navbar />
     </div>
   )
 }
