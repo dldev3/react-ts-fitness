a64700a595eb43eec56fa7ef4d51ea27a503ad4e
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-01-22 05:50:26 +0530

    website guide deletede

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
 
 type Props = {}
 
 const index = (props: Props) => {
+
+    const flexBetween = "flex items-center justify-between";
+
     return (
         <div>Navbar</div>
     )
