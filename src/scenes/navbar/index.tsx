0fb07489399b83085e7ea075263271796262aad7
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-01-25 22:22:09 +0530

    nav styles added

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -14,8 +14,13 @@ const index = (props: Props) => {
                 className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
             >
 
-                <div>
+                <div className={`${flexBetween} mx-auto w-5/6`}>
+                    <div className={`${flexBetween} w-full gap-16`}>
+                        <img alt="logo" src={Logo} />
+                        <div className={`${flexBetween} w-full`}>
 
+                        </div>
+                    </div>
                 </div>
 
             </div>
