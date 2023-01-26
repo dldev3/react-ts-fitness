f89178335a3343d60cc67f2e08a874b338869945
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-01-26 19:24:12 +0530

    links added

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -18,7 +18,17 @@ const index = (props: Props) => {
                     <div className={`${flexBetween} w-full gap-16`}>
                         <img alt="logo" src={Logo} />
                         <div className={`${flexBetween} w-full`}>
+                            <div className={`${flexBetween} gap-8 text-sm`}>
+                                <p>Home</p>
+                                <p>Benefits</p>
+                                <p>Our Classes</p>
+                                <p>Contact Us</p>
+                                <div>
+                                    <p>Sign In</p>
+                                    <button>Become a memeber</button>
+                                </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
